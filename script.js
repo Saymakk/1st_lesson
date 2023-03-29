9020ce5fa500d5a4a72ca94b4f91bdad0a3f7049
Vladislav <vladsarana@gmail.com>
2023-03-29 18:59:09 +0600

    First Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false,
+}
+
+
+const a = prompt('Один из просмотренных недавно фильмов?'),
+      b = prompt('Насколько оцените его?'),
+      c = prompt('Один из просмотренных недавно фильмов?'),
+      d = prompt('Насколько оцените его?');
+
+personalMovieDB.movies[a] = b; 
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
